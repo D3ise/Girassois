16e0f523d1ef40c0025ec13bfdfaaea67bc3fb02
deise <deiseelen17@gmail.com>
2024-03-12 11:47:56 -0300

    o professor explicou sobre a history step, vou começar essa parte amanhã

---
+++ Eclipse/ConfigGamePage.cs
@@ -0,0 +1,12 @@
+public class ConfigGamePage
+{
+public int Id;
+public string? Texto;
+public bool TemImagem;
+public string? UrlDaImagem;
+public string? TextoDaResposta1;
+public string? TextoDaResposta2;
+public int IdNivelRespost1;
+public int IdNivelRespost2;
+public bool EstaMorto = false;
+}
